a3a60b2ee0fc179d2da766dbee89f3e2facb97d7
Vikram Venkataramanan <vikram.venkataramanan@mail.utoronto.ca>
2020-12-01 22:48:09 -0500

    reorder loops

--- day01/part1.go
+++ day01/part1.go
@@ -26,26 +26,23 @@ func PartOne() {
 func PartTwo() {
 	var input = utils.ReadInputFile("01", "input.txt")
 	set := make(map[int]bool)
-	product := make(map[int]int)
 
-	var xAsString, yAsString, zAsString string
-	var x, y, z int
+	var xAsString, yAsString string
+	var x, y int
+
+	for _, xAsString = range input {
+		x, _ = strconv.Atoi(xAsString)
+		set[x] = true
+	}
 
 	for _, xAsString = range input {
 		x, _ = strconv.Atoi(xAsString)
 		for _, yAsString = range input {
 			y, _ = strconv.Atoi(yAsString)
-			z = 2020 - (x + y)
-			set[z] = true
-			product[z] = x * y * z
-		}
-	}
-
-	for _, zAsString = range input {
-		z, _ = strconv.Atoi(zAsString)
-		if _, ok := set[z]; ok {
-			fmt.Printf("Answer %d\n", product[z])
-			return
+			if _, ok := set[2020-(x+y)]; ok {
+				fmt.Printf("Answer %d\n", x*y*(2020-x-y))
+				return
+			}
 		}
 	}
 }
