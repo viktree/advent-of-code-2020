01139a3eed14cbe2539487bdd5fd0c77acddc0f2
Vikram Venkataramanan <vikram.venkataramanan@mail.utoronto.ca>
2020-12-01 23:19:16 -0500

    day 1: split into two files!

--- day01/part1.go
+++ day01/part1.go
@@ -2,43 +2,20 @@ package dayOne
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/viktree/advent-of-code-2020/utils"
 )
 
 func PartOne() {
 
-	var input = utils.ReadInputFile("01", "input.txt")
+	var list = utils.ReadInputFileToInts("01", "input.txt")
 	set := make(map[int]bool)
 
-	var numAsString string
-	for _, numAsString = range input {
-		n, _ := strconv.Atoi(numAsString)
+	for _, n := range list {
 		if _, ok := set[2020-n]; ok {
 			fmt.Printf("Answer %d\n", (n)*(2020-n))
 			return
 		}
 		set[n] = true
-	}
-}
-
-func PartTwo() {
-	var input = utils.ReadInputFile("01", "input.txt")
-	set := make(map[int]bool)
-
-	var xAsString, yAsString string
-	var x, y int
-
-	for _, xAsString = range input {
-		x, _ = strconv.Atoi(xAsString)
-		set[x] = true
-		for _, yAsString = range input {
-			y, _ = strconv.Atoi(yAsString)
-			if _, ok := set[2020-(x+y)]; ok {
-				fmt.Printf("Answer %d\n", x*y*(2020-x-y))
-				return
-			}
-		}
 	}
 }
--- utils/readInput.go
+++ utils/readInput.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 func ReadInputFile(day string, fileName string) []string {
@@ -28,6 +29,33 @@ func ReadInputFile(day string, fileName string) []string {
 	fileScanner := bufio.NewScanner(f)
 	for fileScanner.Scan() {
 		lines = append(lines, fileScanner.Text())
+	}
+
+	return lines
+}
+
+func ReadInputFileToInts(day string, fileName string) []int {
+	relativeFilePath := "./day" + day + "/" + fileName
+
+	// convert to absolute path
+	absPath, err := filepath.Abs(relativeFilePath)
+	if err != nil {
+		log.Fatal("Error: failed to get entire filepath.")
+	}
+
+	// read from file
+	f, err := os.Open(absPath)
+	if err != nil {
+		log.Fatal("Error: failed to open file.")
+		os.Exit(2)
+	}
+	defer f.Close()
+
+	var lines []int
+	fileScanner := bufio.NewScanner(f)
+	for fileScanner.Scan() {
+		n, _ := strconv.Atoi(fileScanner.Text())
+		lines = append(lines, n)
 	}
 
 	return lines
